3d3d04dc39dca211b64bd1f7e01be39350576ba5
msherga <63604994+msherga@users.noreply.github.com>
2025-07-01 21:39:43 -0500

    Update run.sh

--- Slideshow-Server/run.sh
+++ Slideshow-Server/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bashio
 
-echo "[INFO] Nanit stream add-on starting..."
+echo "[INFO] Slideshow Server add-on starting..."
 cd /local/Slideshow
 python3 -m http.server 8000 --bind 0.0.0.0
